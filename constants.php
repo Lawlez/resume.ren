e3dfb186725d6c498f7845129c97a011e90b01b9
Dominik <dominik@lawlez.ch>
2019-07-31 11:23:28 +0200

    basic visitor logs + constants

---
+++ constants.php
@@ -0,0 +1,12 @@
+<?php
+//CONSTANTS
+
+define('DOC_ROOT',$_SERVER['DOCUMENT_ROOT']."/" );
+define('DELAY_MAIN', "2");
+
+//Links
+define('IG_PHOTO', "https://instagram.com/lawlez_/");
+define('IG_REN', "https://instagram.com/lawlez_render/");
+define('LINKEDIN', "https://linkedin.com/dominik-feger/");
+define('ART_PORTFOLIO', "http://lawlez.myportfolio.com/");
+?>
--- index.php
+++ index.php
@@ -1,6 +1,63 @@
+<!-- get constants & error reporting-->
+<?php
+require 'constants.php';
+error_reporting(E_ALL);
+?>
 <!DOCTYPE html>
 <html lang="en">
+<!-- Log collect-->
+<?php
+write_log("visit");
+define("DEFAULT_LOG","logs/weblog.lwlx");
+ 
+function write_log($message, $logfile = 'logs/weblog.lwlx') {
+  if($logfile == '') {
+    if (defined(DEFAULT_LOG) == TRUE) {
+        $logfile = DEFAULT_LOG;
+    }
+    else {
+        error_log('No log file defined!',0);
+        return array(status => false, message => 'No log file defined!');
+    }
+  }
 
+if(isset($_COOKIE['countVisit'])){
+$countVisit = $_COOKIE['countVisit'];
+}
+if(isset($_COOKIE['lastVisit'])){
+$lastVisit = $_COOKIE['lastVisit'];
+
+}
+  if(isset($_COOKIE['cpuser'])){
+$cpuser = $_COOKIE['cpuser'];
+
+}
+  setcookie('cpuser', date("d-m-Y"),USER_COOKIE ,PASS_COOKIE, time()+72000);//set usr cookie
+// set cookie for countVisit
+setcookie('countVisit', $countVisit++,  time()+360000);
+// set cookie for last visit
+setcookie('lastVisit', date("d-m-Y H:i:s"),  time()+360000);
+
+
+  if( ($time = $_SERVER['REQUEST_TIME']) == '') {
+    $time = time();
+  }
+ 
+  if( ($remote_addr = $_SERVER['REMOTE_ADDR']) == '') {
+    $remote_addr = "REMOTE_ADDR_UNKNOWN";
+  }
+ 
+  if( ($request_uri = $_SERVER['REQUEST_URI']) == '') {
+    $request_uri = "REQUEST_URI_UNKNOWN";
+  }
+  $date = date("Y-m-d H:i:s", $time);
+  if($fd = @fopen($logfile, "a")) {
+    $result = fwrite($fd,"Date: ".$date."| IP: ".$remote_addr."| usr/loggedin: ".USER_COOKIE."/".LOGGED_IN_COOKIE."| Req-Script: ".$request_uri."| Msg: ".$message." | Last Visit:".$lastVisit." | Visit Count: [".$countVisit."]\n");
+    fclose($fd);
+
+}
+}
+?>
 <head>
 
   <meta charset="utf-8">
