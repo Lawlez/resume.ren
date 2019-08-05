c88f81a3deede567a44e7eaa943531dc892fa119
Dominik <dominik@lawlez.ch>
2019-08-05 13:41:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -370,7 +370,7 @@ var myChart = new Chart(ctx, {
         labels: ['Json', 'PHP', 'JS', 'jQuery', 'HTML', 'CSS', 'mySQL', 'Solidity', 'Databases', 'CMS', 'Bootstrap', 'Magento', 'Django', 'Kali Linux', 'Apache', 'Git', 'SEO', 'Social Engineering', 'Penetration Testing', 'XSS', 'SMM', 'SEM', 'Debugging', 'Digital Design' ],
         datasets: [{
             label: '% Proficiency',
-            data: [90, 86, 30, 50, 20, 30,20,40,80,80,86,90,50,10,68,98,78,69,89,84,85,78,54,45,33],
+            data: [90, 86, 30, 50, 60, 30,70,40,80,80,86,90,50,80,68,98,78,69,89,84,85,78,54,45],
             pointbackgroundColor: [
                 'rgba(255, 99, 132, 0.9)'
                 
