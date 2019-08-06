7feffc2510e391bdf1378e9dbfcf5e79ba7807a4
Lawlez <dk.feger@gmail.com>
2019-08-06 16:41:32 +0200

    radar scale

--- index.php
+++ index.php
@@ -369,9 +369,13 @@ var myChart = new Chart(ctx, {
     type: 'radar',
     options: {
     scale: {
-        // Hides the scale
-        display: true
-    },
+    ticks: {
+        beginAtZero: true,
+        max: 100,
+        min: 0,
+        stepSize: 5
+    }
+},
     layout: {
             padding: {
                 left: 40,
